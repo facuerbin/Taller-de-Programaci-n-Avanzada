d52b5481a8265c2950fcddaf50311cf97a4a2c57
Facu Erbin <facuerbin@gmail.com>
2021-09-02 17:38:25 -0300

    Creo el proyecto de API Springboot para el TP4

---
+++ apiPersona/src/test/java/com/example/apiPersona/ApiPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiPersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
