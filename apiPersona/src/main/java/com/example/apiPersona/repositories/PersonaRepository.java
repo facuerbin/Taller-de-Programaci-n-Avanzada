b7a60eb1734715641de6443782ef199be45374f7
Facu Erbin <facuerbin@gmail.com>
2021-09-06 19:16:39 -0300

    Agrego Controladores y Repositories de Persona. Videos de API Springboot hasta el 7

---
+++ apiPersona/src/main/java/com/example/apiPersona/repositories/PersonaRepository.java
@@ -0,0 +1,11 @@
+package com.example.apiPersona.repositories;
+
+import com.example.apiPersona.entities.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+}
+
+
